fc0dcc30490c3dee9178e04714ddc7449891aa11
momi77 <anotp96@gmail.com>
2026-01-08 22:08:17 +0600

    lessons commit

---
+++ cinema_api/films/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class Director(models.Model):
+    fio = models.CharField(max_length=255)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return self.fio
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Film(models.Model):
+    director = models.ForeignKey(Director, on_delete=models.CASCADE,
+                                  null=True)
+    genres = models.ManyToManyField(Genre, blank=True)
+    title = models.CharField(max_length=255)
+    text = models.TextField(null=True, blank=True)
+    rating = models.FloatField()
+    release_year = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_hit = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+    @property
+    def genre_names(self):
+        return [i.name for i in self.genres.all()]
+    
+
+class Review(models.Model):
+    text = models.TextField()
+    star = models.IntegerField(choices=((i, i) for i in range(1, 11)), default=7)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE,
+                              related_name='reviews')
+    
+    def __str__(self):
+        return self.text
